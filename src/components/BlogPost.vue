8748b232a1dfec366710ed3c022b17355e29428f
Ekaterina Larionova <ekaterinalarionova25@gmail.com>
2022-12-18 20:49:05 +0300

    added comments

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -15,10 +15,10 @@
 
       <section class="post__body rte" v-html="content"></section>
 
-<!--      <footer class="post__footer">-->
-<!--        <vue-disqus v-if="commentsReady" shortname="vue-blog-demo"-->
-<!--          :key="post" :identifier="post" :url="`https://vue-blog-demo.netlify.com/read/${post}`"/>-->
-<!--      </footer>-->
+      <footer class="post__footer">
+        <vue-disqus v-if="commentsReady" shortname="nir-simple"
+          :key="post" :identifier="post" :url="`https://nir-project-simple.onrender.com/read/${post}`"/>
+      </footer>
     </article>
   </transition>
 </template>
