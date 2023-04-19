a1fd789cf3f76de6d63824307bbe95930eeb9aab
Ekaterina <ekaterinalarionova25@gmail.com>
2023-04-19 10:30:32 +0300

    supabase added

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://txrpiccpqvudzoxdczjv.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4cnBpY2NwcXZ1ZHpveGRjemp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE4NDU4MjUsImV4cCI6MTk5NzQyMTgyNX0.rQBvBmFSU4tN6W65hyKInmJCkhI68QPyIb0jTc5UaQU')
