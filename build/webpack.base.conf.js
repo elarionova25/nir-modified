5a50d72a3d957dde3f611584797c8c2f871a7cd1
Ekaterina <ekaterinalarionova25@gmail.com>
2023-04-20 14:01:57 +0300

    webpack config change

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -45,7 +45,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/@supabase')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
